4dd6af84cac26a3e741974990cec9c877f88e699
madoka5555 <73727829+madoka5555@users.noreply.github.com>
2021-01-16 16:31:23 +0900

    ユーザー認証機能の実装 ログイン・ログアウト

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,21 @@
+class PrototypesController < ApplicationController
+
+  def index
+    @prototypes
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+end
